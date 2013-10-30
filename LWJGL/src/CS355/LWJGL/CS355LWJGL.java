4328ad9a2f56d3c9722662f3d9c0e0dbd1613c28
Paula Chen <paula.chenn@gmail.com>
2013-10-29 21:25:03 -0600

    maybe done lab4

--- LWJGL/src/CS355/LWJGL/CS355LWJGL.java
+++ LWJGL/src/CS355/LWJGL/CS355LWJGL.java
@@ -15,7 +15,7 @@ public class CS355LWJGL
     try 
     {
       main = new LWJGLSandbox();
-      main.create(new SOLUTION());
+      main.create(new StudentLWJGLController());
       main.run();
     }
     catch(Exception ex) 
--- LWJGL/src/CS355/LWJGL/StudentLWJGLController.java
+++ LWJGL/src/CS355/LWJGL/StudentLWJGLController.java
@@ -9,7 +9,10 @@ package CS355.LWJGL;
 //Therefore, if a command appears in this list, you probably need it.
 //If it doesn't appear in this list, you probably don't.
 //Of course, your milage may vary. Don't feel restricted by this list of imports.
+import java.util.Iterator;
+
 import org.lwjgl.input.Keyboard;
+
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_LINES;
 import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
@@ -28,44 +31,74 @@ import static org.lwjgl.opengl.GL11.glViewport;
 import static org.lwjgl.opengl.GL11.glOrtho;
 import static org.lwjgl.util.glu.GLU.gluPerspective;
 
+
+import static org.lwjgl.util.glu.GLU.gluLookAt;
+import static org.lwjgl.opengl.GL11.glVertex3f;
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.util.glu.GLU.gluOrtho2D;
+import static org.lwjgl.opengl.GL11.glClearDepth;
 /**
  *
  * @author Brennan Smith
  */
 public class StudentLWJGLController implements CS355LWJGLController 
 {
-  
-  //This is a model of a house.
-  //It has a single method that returns an iterator full of Line3Ds.
-  //A "Line3D" is a wrapper class around two Point2Ds.
-  //It should all be fairly intuitive if you look at those classes.
-  //If not, I apologize.
-  private WireFrame model = new HouseModel();
+	private WireFrame model = new HouseModel();
+	
+	//camera
+	private float cameraX = 0;
+	private float cameraY = -5;
+	private float cameraZ = -20;
+	private float cameraDirection = 0;
+	private float aspect = 640 / 480;
 
-  //This method is called to "resize" the viewport to match the screen.
-  //When you first start, have it be in perspective mode.
-  @Override
-  public void resizeGL() 
-  {
-
-  }
+	//This method is called to "resize" the viewport to match the screen.
+	//When you first start, have it be in perspective mode.
+	@Override
+	public void resizeGL() 
+	{
+		glViewport(0,0,640,480);
+		
+		// Set the aspect ratio of the clipping volume to match the viewport
+		glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
+		glLoadIdentity();             // Reset
+		// Enable perspective projection with fovy, aspect, zNear and zFar
+		gluPerspective(50.0f, aspect, 0.1f, 100f);
+	}
 
     @Override
     public void update() 
     {
-        
+        // no need for this lab
     }
 
-    //This is called every frame, and should be responsible for keyboard updates.
-    //An example keyboard event is captured below.
-    //The "Keyboard" static class should contain everything you need to finish
-    // this up.
     @Override
-    public void updateKeyboard() 
+    public void updateKeyboard()
     {
-        if(Keyboard.isKeyDown(Keyboard.KEY_W)) 
+        if(Keyboard.isKeyDown(Keyboard.KEY_A)) cameraX++;
+        else if(Keyboard.isKeyDown(Keyboard.KEY_D)) cameraX--;
+        else if(Keyboard.isKeyDown(Keyboard.KEY_W)) cameraZ++;
+        else if(Keyboard.isKeyDown(Keyboard.KEY_S)) cameraZ--;
+        else if(Keyboard.isKeyDown(Keyboard.KEY_Q)) cameraDirection--;
+        else if(Keyboard.isKeyDown(Keyboard.KEY_E)) cameraDirection++;
+        else if(Keyboard.isKeyDown(Keyboard.KEY_R)) cameraY--;
+        else if(Keyboard.isKeyDown(Keyboard.KEY_F)) cameraY++;
+        else if(Keyboard.isKeyDown(Keyboard.KEY_O)) 
         {
-            System.out.println("You are pressing W!");
+			// Set the aspect ratio of the clipping volume to match the viewport
+			glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
+			glLoadIdentity();             // Reset
+		    // aspect >= 1, set the height from -1 to 1, with larger width
+		    glOrtho(-10.0 * aspect, 10.0 * aspect, -10.0, 10.0, 0.1, 100);
+        }
+        else if(Keyboard.isKeyDown(Keyboard.KEY_P)) 
+        {
+    		// Set the aspect ratio of the clipping volume to match the viewport
+    		glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
+    		glLoadIdentity();             // Reset
+    		// Enable perspective projection with fovy, aspect, zNear and zFar
+    		gluPerspective(50.0f, aspect, 0.1f, 100f);
         }
     }
 
@@ -75,8 +108,22 @@ public class StudentLWJGLController implements CS355LWJGLController
     {
         //This clears the screen.
         glClear(GL_COLOR_BUFFER_BIT);
+        glClearDepth(1.0f); // Set background depth to farthest
         
-        //Do your drawing here.
+        glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
+	    glLoadIdentity();                  // Reset the model-view matrix
+	    glTranslatef(cameraX, cameraY, cameraZ);  // Move left and into the screen
+	    glRotatef(cameraDirection,0.0f,1.0f,0.0f);
+	    
+        Iterator<Line3D> linesItr = model.getLines();
+        glBegin(GL_LINES);
+        while(linesItr.hasNext()){
+        	Line3D line = linesItr.next();
+        	glColor3f(1.0f, 1.0f, 1.0f);
+        	glVertex3d(line.start.x,line.start.y,line.start.z);
+        	glVertex3d(line.end.x,line.end.y,line.end.z);
+        }
+        glEnd();
     }
     
 }
